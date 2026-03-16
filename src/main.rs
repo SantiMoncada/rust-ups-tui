5101ff6015f698fe81815dbf09ed1d678b12765b
santimoncada <santiago.m.a@hotmail.com>
2026-03-16 02:56:33 +0100

    parsed data from ups

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use rusqlite::Connection;
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
+use std::time::{SystemTime, UNIX_EPOCH};
 
 const IP: &str = "localhost";
 const PORT: &str = "3493";
+
+#[derive(Debug)]
+enum NutStatus {
+    OL,
+    OB,
+    LB,
+}
+
+#[derive(Debug)]
+struct DataEntry {
+    charge: u8,
+    load: u16,
+    status: NutStatus,
+    outlet: bool,
+    timestamp: u64,
+}
+
+fn init_db() {}
+
+fn get_ups_data() {}
 
 fn main() {
     println!("Connecting to DB...");
@@ -57,6 +78,19 @@ fn main() {
 
     let reader = BufReader::new(&stream);
 
+    let now: u64 = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let mut data = DataEntry {
+        timestamp: now,
+        charge: 0,
+        load: 0,
+        status: NutStatus::OL,
+        outlet: true,
+    };
+
     for line in reader.lines() {
         let line = line.expect("Error reading from stream");
         if line.starts_with("END LIST") {
@@ -65,22 +99,45 @@ fn main() {
 
         match line {
             l if l.contains("battery.charge") => {
-                let value = l.split_whitespace().last().unwrap();
-                println!("{}", value);
+                let value = l.split_whitespace().last().expect("Failed to parse charge");
+                data.charge = value
+                    .trim_matches('"')
+                    .parse()
+                    .expect("Failed to parse charge");
             }
             l if l.contains("ups.load") => {
-                let value = l.split_whitespace().last().unwrap();
-                println!("{}", value);
+                let value = l.split_whitespace().last().expect("Failed to parse load");
+                data.load = value
+                    .trim_matches('"')
+                    .parse()
+                    .expect("Failed to parse charge");
             }
             l if l.contains("ups.status") => {
-                let value = l.split_whitespace().last().unwrap();
-                println!("{}", value);
+                let value = l.split_whitespace().last().expect("Failed to parse status");
+                data.status = match value.trim_matches('"') {
+                    "OL" => NutStatus::OL,
+                    "OB" => NutStatus::OB,
+                    "LB" => NutStatus::LB,
+                    _ => {
+                        println!("Error Parsing Status");
+                        return;
+                    }
+                }
             }
             l if l.contains("outlet.1.status") => {
-                let value = l.split_whitespace().last().unwrap();
-                println!("{}", value);
+                let value = l.split_whitespace().last().expect("Failed to parse Outlet");
+                data.outlet = match value.trim_matches('"') {
+                    "on" => true,
+                    "off" => false,
+                    _ => {
+                        println!("Error parsing Outlet");
+                        return;
+                    }
+                }
             }
             _ => {}
         };
     }
+
+    println!("structed data {data:?}");
 }
